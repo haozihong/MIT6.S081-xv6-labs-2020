957b1a7d7b2baaa0cacb75a72fb87f4ed112f48f
Zihong Hao <haozihong@hotmail.com>
2022-12-30 13:34:48 -0500

    lab lazy-2 Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,18 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    // page fault
+    printf("page fault: scause=%d va=%d\n", r_scause(), r_stval());
+
+    uint64 va = PGROUNDDOWN(r_stval());
+    char *mem = kalloc();
+    if(mem == 0) panic("page fault kalloc");
+    memset(mem, 0, PGSIZE);
+    if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree(mem);
+      panic("page fault mappages");
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
